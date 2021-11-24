81fae80ab360bbcd60737a2e5ebe9eab1f913fb8
MrYapetzku <72166768+MrYapetzku@users.noreply.github.com>
2021-11-24 22:14:14 +0300

    Init

---
+++ Weapon.cs
@@ -0,0 +1,11 @@
+﻿namespace Grinding
+{
+    internal class Weapon
+    {
+        private int _bullets;
+
+        public bool CanShoot() => _bullets > 0;
+
+        public void Shoot() => _bullets -= 1;
+    }
+}
