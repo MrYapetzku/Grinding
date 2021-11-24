bcd1f25f60f3b9f5fdc8c1fe4b6ff255454eedf1
MrYapetzku <72166768+MrYapetzku@users.noreply.github.com>
2021-11-24 21:24:10 +0300

    Init

---
+++ Task_2.cs
@@ -0,0 +1,15 @@
+﻿namespace Grinding
+{
+    internal class Task_2
+    {
+        public static int MakeValidNumber(int a, int b, int c)
+        {
+            if (a < b)
+                return b;
+            else if (a > c)
+                return c;
+            else
+                return a;
+        }
+    }
+}
