d8a9429c31fc302ff7e1bdc8943455832ac164a5
MrYapetzku <72166768+MrYapetzku@users.noreply.github.com>
2021-12-15 22:27:50 +0300

    magic number replacement

--- Weapon.cs
+++ Weapon.cs
@@ -2,10 +2,12 @@
 {
     internal class Weapon
     {
+        private const int BULLETSPERSHOT = 1;
+
         private int _bullets;
 
-        public bool CanShoot() => _bullets > 0;
+        public bool CanShoot() => _bullets >= BULLETSPERSHOT;
 
-        public void Shoot() => _bullets -= 1;
+        public void Shoot() => _bullets -= BULLETSPERSHOT;
     }
 }
